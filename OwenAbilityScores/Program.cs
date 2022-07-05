232dfacbb72483341ba9f370618056ecb17b454f
Owen <owen.bayliss@sky.com>
2022-07-05 22:20:57 +0100

    Add project files.

---
+++ OwenAbilityScores/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace OwenAbilityScores
+{
+    class AbilityScoreCalculator
+    {
+        public int RollResult = 14;
+        public double DivideBy = 1.75;
+        public int AddAmount = 2;
+        public int Minimum = 3;
+        public int Score;
+
+        public void CalculateAbilityScore()
+        {
+            double divided = RollResult / DivideBy;
+            int added = AddAmount += (int)divided;
+            if (added < Minimum)
+            {
+                Score = Minimum;
+            }
+            else
+            {
+                Score = added;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AbilityScoreCalculator calculator = new AbilityScoreCalculator();
+            while (true)
+            {
+                calculator.RollResult = ReadInt(calculator.RollResult, "Starting 4d6 roll");
+                calculator.DivideBy = ReadDouble(calculator.DivideBy, "Divide by");
+                calculator.AddAmount = ReadInt(calculator.AddAmount, "Add amount");
+                calculator.Minimum = ReadInt(calculator.Minimum, "Minimum");
+                calculator.CalculateAbilityScore();
+                Console.WriteLine("Calculated ability score: " + calculator.Score);
+                Console.WriteLine("Press Q to quit, any other key to continue");
+                char keyChar = Console.ReadKey(true).KeyChar;
+                if ((keyChar == 'Q' || keyChar == 'q')) return;
+            }
+        }
+
+        static double ReadInt(int lastUsedValue, string prompt)
+        {
+            Console.WriteLine(prompt + " [" + lastUsedValue + "]: ");
+            string inputValue = Console.ReadLine();
+            if (int.TryParse(inputValue, out int chosenValue))
+            {
+                Console.WriteLine(" using value" + chosenValue);
+                return lastUsedValue;
+            }
+            else
+            {
+                Console.WriteLine(" using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+
+        static double ReadDouble(double lastUsedValue, string prompt)
+        {
+            Console.WriteLine(prompt + " [" + lastUsedValue + "]: ");
+            string inputValue = Console.ReadLine();
+            if (double.TryParse(inputValue, out double chosenValue))
+            {
+                Console.WriteLine(" using value " + chosenValue);
+                return chosenValue;
+            }
+            else
+            {
+                Console.WriteLine(" using default value " + lastUsedValue);
+                return lastUsedValue;
+            }
+        }
+    }
+}
